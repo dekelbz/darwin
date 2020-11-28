922389bfedc9b8dfec0e33b978460cd582f9a73c
dekelbz <dekelbz@gmail.com>
2020-11-28 23:17:49 +0200

    adding user controller unit tests

--- src/test/java/com/dekel/darwin/users/controller/UserControllerTest.java
+++ src/test/java/com/dekel/darwin/users/controller/UserControllerTest.java
@@ -12,23 +12,30 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.arrayContainingInAnyOrder;
 import static org.hamcrest.Matchers.hasItem;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.verifyNoInteractions;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(UserController.class)
 class UserControllerTest {
 
+    public static final String USER_URL = "/user";
     @Autowired
     private MockMvc mvc;
 
@@ -52,9 +59,7 @@ class UserControllerTest {
                 .build();
 
         //when
-        mvc.perform(MockMvcRequestBuilders.post("/user")
-                .contentType(MediaType.APPLICATION_JSON)
-                .accept(MediaType.APPLICATION_JSON)
+        mvc.perform(post(USER_URL)
                 .params(getBody(userDTO)))
                 .andExpect(status().isOk());
 
@@ -78,26 +83,69 @@ class UserControllerTest {
                 .build();
 
         //when
-        MvcResult mvcResult = mvc.perform(MockMvcRequestBuilders.post("/user")
-                .contentType(MediaType.APPLICATION_JSON)
-                .accept(MediaType.APPLICATION_JSON)
+        mvc.perform(post(USER_URL)
                 .params(getBody(userDTO)))
                 .andExpect(status().is4xxClientError())
-                .andReturn();
+                .andExpect(content().json(objectMapper.writeValueAsString(Collections.singletonList("{error.empty.department}"))));
 
         //then
-        List<String> result = objectMapper.readerForListOf(String.class)
-                .readValue(mvcResult.getResponse()
-                .getContentAsByteArray());
-        assertThat(result, hasItem("{error.empty.department}"));
-
         verifyNoInteractions(userPresentationLayer);
+    }
+
+    @Test
+    public void shouldGetUser() throws Exception {
+        //given
+        UserDTO userDTO = UserDTO.builder()
+                .firstName("user first name")
+                .build();
+
+        String userEmail = "correct@user.com";
+        given(userPresentationLayer.getByEmail(userEmail))
+                .willReturn(Optional.of(userDTO));
+
+        //when + then
+        mvc.perform(get(USER_URL)
+                .param("email", userEmail))
+                .andExpect(status().isOk())
+                .andExpect(content().json(objectMapper.writeValueAsString(userDTO)));
+    }
+
+    @Test
+    public void shouldNotGetUser_notFound() throws Exception {
+        //when + then
+        mvc.perform(get(USER_URL)
+                .param("email", "not@found.com"))
+                .andExpect(status().is4xxClientError())
+                .andReturn();
+    }
+
+    @Test
+    public void shouldDelete() throws Exception {
+        //given
+        String userEmail = "email@delete.com";
+        given(userPresentationLayer.deleteByEmail(userEmail))
+                .willReturn(true);
+
+        //when + then
+        mvc.perform(delete(USER_URL)
+                .param("email", userEmail))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void shouldNotDelete_notFound() throws Exception {
+        //when + then
+        mvc.perform(delete(USER_URL)
+                .param("email", "user@email.com"))
+                .andExpect(status().is4xxClientError());
     }
 
     private MultiValueMap<String, String> getBody(UserDTO userDTO) throws JsonProcessingException {
         return objectMapper.readerFor(Map.class)
                 .<Map<String, String>>readValue(objectMapper.writeValueAsString(userDTO))
                 .entrySet().stream()
-                .collect(LinkedMultiValueMap::new, (map, entry) -> map.add(entry.getKey(), entry.getValue()), (map, entry) -> {});
+                .collect(LinkedMultiValueMap::new,
+                        (map, entry) -> map.add(entry.getKey(), entry.getValue()),
+                        (map, entry) -> {});
     }
 }
